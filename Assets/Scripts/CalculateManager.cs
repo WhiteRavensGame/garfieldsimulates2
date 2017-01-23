7dfdbb22215aa289e5accb12be94757e426e4a8a
Charles Cue <charles@v2g.io>
2017-01-23 02:10:47 -0800

    Implemented the Ally Level Abilites Cost Wall. Still working on pattern-behavior AI.

--- Assets/Scripts/CalculateManager.cs
+++ Assets/Scripts/CalculateManager.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Google2u;
+using System.Collections.Generic;
 
 
 public class CalculateManager : MonoBehaviour {
@@ -65,6 +66,27 @@ public class CalculateManager : MonoBehaviour {
 	private float ally20Power = 0;
 	private int garfieldLevel = 1;
 	private float garfieldPower;
+
+	private List<int> ally1Upgrades;
+	private List<int> ally2Upgrades;
+	private List<int> ally3Upgrades;
+	private List<int> ally4Upgrades;
+	private List<int> ally5Upgrades;
+	private List<int> ally6Upgrades;
+	private List<int> ally7Upgrades;
+	private List<int> ally8Upgrades;
+	private List<int> ally9Upgrades;
+	private List<int> ally10Upgrades;
+	private List<int> ally11Upgrades;
+	private List<int> ally12Upgrades;
+	private List<int> ally13Upgrades;
+	private List<int> ally14Upgrades;
+	private List<int> ally15Upgrades;
+	private List<int> ally16Upgrades;
+	private List<int> ally17Upgrades;
+	private List<int> ally18Upgrades;
+	private List<int> ally19Upgrades;
+	private List<int> ally20Upgrades;
 
 	private float currentMoney = 0;
 
@@ -143,6 +165,28 @@ public class CalculateManager : MonoBehaviour {
 		ally20Level = 0;
 		ally20Power = 0;
 		garfieldLevel = 1;
+
+		ally1Upgrades = new List<int>();
+		ally2Upgrades = new List<int>();
+		ally3Upgrades = new List<int>();
+		ally4Upgrades = new List<int>();
+		ally5Upgrades = new List<int>();
+		ally6Upgrades = new List<int>();
+		ally7Upgrades = new List<int>();
+		ally8Upgrades = new List<int>();
+		ally9Upgrades = new List<int>();
+		ally10Upgrades = new List<int>();
+		ally11Upgrades = new List<int>();
+		ally12Upgrades = new List<int>();
+		ally13Upgrades = new List<int>();
+		ally14Upgrades = new List<int>();
+		ally15Upgrades = new List<int>();
+		ally16Upgrades = new List<int>();
+		ally17Upgrades = new List<int>();
+		ally18Upgrades = new List<int>();
+		ally19Upgrades = new List<int>();
+		ally20Upgrades = new List<int>();
+
 		AllyStatsRow meh = AllyStats.Instance.GetRow("ID_0");
 		garfieldPower = meh._garfieldAttack;
 
@@ -196,71 +240,71 @@ public class CalculateManager : MonoBehaviour {
 			if(currentGameAction == null)
 			{
 				BuyAlly(0, garfieldLevel + 1);
-				BuyAlly(1, ally1Level + 1);
-				BuyAlly(2, ally2Level + 1);
-				BuyAlly(3, ally3Level + 1);
-				BuyAlly(4, ally4Level + 1);
-				BuyAlly(5, ally5Level + 1);
-				BuyAlly(6, ally6Level + 1);
-				BuyAlly(7, ally7Level + 1);
-				BuyAlly(8, ally8Level + 1);
-				BuyAlly(9, ally9Level + 1);
-				BuyAlly(10, ally10Level + 1);
-				BuyAlly(11, ally11Level + 1);
-				BuyAlly(12, ally12Level + 1);
-				BuyAlly(13, ally13Level + 1);
-				BuyAlly(14, ally14Level + 1);
-				BuyAlly(15, ally15Level + 1);
-				BuyAlly(16, ally16Level + 1);
-				BuyAlly(17, ally17Level + 1);
-				BuyAlly(18, ally18Level + 1);
-				BuyAlly(19, ally19Level + 1);
-				BuyAlly(20, ally20Level + 1);
+				if(IsAbilityBuyWall(ally1Level) && !ally1Upgrades.Contains(ally1Level)) BuyAllyAbility(1, ally1Level); else BuyAlly(1, ally1Level + 1);
+				if(IsAbilityBuyWall(ally2Level) && !ally2Upgrades.Contains(ally2Level)) BuyAllyAbility(2, ally2Level); else BuyAlly(2, ally2Level + 1);
+				if(IsAbilityBuyWall(ally3Level) && !ally3Upgrades.Contains(ally3Level)) BuyAllyAbility(3, ally3Level); else BuyAlly(3, ally3Level + 1);
+				if(IsAbilityBuyWall(ally4Level) && !ally4Upgrades.Contains(ally4Level)) BuyAllyAbility(4, ally4Level); else BuyAlly(4, ally4Level + 1);
+				if(IsAbilityBuyWall(ally5Level) && !ally5Upgrades.Contains(ally5Level)) BuyAllyAbility(5, ally5Level); else BuyAlly(5, ally5Level + 1);
+				if(IsAbilityBuyWall(ally6Level) && !ally6Upgrades.Contains(ally6Level)) BuyAllyAbility(6, ally6Level); else BuyAlly(6, ally6Level + 1);
+				if(IsAbilityBuyWall(ally7Level) && !ally7Upgrades.Contains(ally7Level)) BuyAllyAbility(7, ally7Level); else BuyAlly(7, ally7Level + 1);
+				if(IsAbilityBuyWall(ally8Level) && !ally8Upgrades.Contains(ally8Level)) BuyAllyAbility(8, ally8Level); else BuyAlly(8, ally8Level + 1);
+				if(IsAbilityBuyWall(ally9Level) && !ally9Upgrades.Contains(ally9Level)) BuyAllyAbility(9, ally9Level); else BuyAlly(9, ally9Level + 1);
+				if(IsAbilityBuyWall(ally10Level) && !ally10Upgrades.Contains(ally10Level)) BuyAllyAbility(10, ally10Level); else BuyAlly(10, ally10Level + 1);
+				if(IsAbilityBuyWall(ally11Level) && !ally11Upgrades.Contains(ally11Level)) BuyAllyAbility(11, ally11Level); else BuyAlly(11, ally11Level + 1);
+				if(IsAbilityBuyWall(ally12Level) && !ally12Upgrades.Contains(ally12Level)) BuyAllyAbility(12, ally12Level); else BuyAlly(12, ally12Level + 1);
+				if(IsAbilityBuyWall(ally13Level) && !ally13Upgrades.Contains(ally13Level)) BuyAllyAbility(13, ally13Level); else BuyAlly(13, ally13Level + 1);
+				if(IsAbilityBuyWall(ally14Level) && !ally14Upgrades.Contains(ally14Level)) BuyAllyAbility(14, ally14Level); else BuyAlly(14, ally14Level + 1);
+				if(IsAbilityBuyWall(ally15Level) && !ally15Upgrades.Contains(ally15Level)) BuyAllyAbility(15, ally15Level); else BuyAlly(15, ally15Level + 1);
+				if(IsAbilityBuyWall(ally16Level) && !ally16Upgrades.Contains(ally16Level)) BuyAllyAbility(16, ally16Level); else BuyAlly(16, ally16Level + 1);
+				if(IsAbilityBuyWall(ally17Level) && !ally17Upgrades.Contains(ally17Level)) BuyAllyAbility(17, ally17Level); else BuyAlly(17, ally17Level + 1);
+				if(IsAbilityBuyWall(ally18Level) && !ally18Upgrades.Contains(ally18Level)) BuyAllyAbility(18, ally18Level); else BuyAlly(18, ally18Level + 1);
+				if(IsAbilityBuyWall(ally19Level) && !ally19Upgrades.Contains(ally19Level)) BuyAllyAbility(19, ally19Level); else BuyAlly(19, ally19Level + 1);
+				if(IsAbilityBuyWall(ally20Level) && !ally20Upgrades.Contains(ally20Level)) BuyAllyAbility(20, ally20Level); else BuyAlly(20, ally20Level + 1);
 			}
 			else //2 - Do the current game action required.
 			{
 				if(currentGameAction.action == Action.BuyTapPower)
 					BuyAlly(0, garfieldLevel + 1);
 				else if(currentGameAction.action == Action.BuyAlly1)
-					BuyAlly(1, ally1Level + 1);
+					if(IsAbilityBuyWall(ally1Level) && !ally1Upgrades.Contains(ally1Level)) BuyAllyAbility(1, ally1Level); else BuyAlly(1, ally1Level + 1);
 				else if(currentGameAction.action == Action.BuyAlly2)
-					BuyAlly(2, ally2Level + 1);
+					if(IsAbilityBuyWall(ally2Level) && !ally2Upgrades.Contains(ally2Level)) BuyAllyAbility(2, ally2Level); else BuyAlly(2, ally2Level + 1);
 				else if(currentGameAction.action == Action.BuyAlly3)
-					BuyAlly(3, ally3Level + 1);
+					if(IsAbilityBuyWall(ally3Level) && !ally3Upgrades.Contains(ally3Level)) BuyAllyAbility(3, ally3Level); else BuyAlly(3, ally3Level + 1);
 				else if(currentGameAction.action == Action.BuyAlly4)
-					BuyAlly(4, ally4Level + 1);
+					if(IsAbilityBuyWall(ally4Level) && !ally4Upgrades.Contains(ally4Level)) BuyAllyAbility(4, ally4Level); else BuyAlly(4, ally4Level + 1);
 				else if(currentGameAction.action == Action.BuyAlly5)
-					BuyAlly(5, ally5Level + 1);
+					if(IsAbilityBuyWall(ally5Level) && !ally5Upgrades.Contains(ally5Level)) BuyAllyAbility(5, ally5Level); else BuyAlly(5, ally5Level + 5);
 				else if(currentGameAction.action == Action.BuyAlly6)
-					BuyAlly(6, ally6Level + 1);
+					if(IsAbilityBuyWall(ally6Level) && !ally6Upgrades.Contains(ally6Level)) BuyAllyAbility(6, ally6Level); else BuyAlly(6, ally6Level + 1);
 				else if(currentGameAction.action == Action.BuyAlly7)
-					BuyAlly(7, ally7Level + 1);
+					if(IsAbilityBuyWall(ally7Level) && !ally7Upgrades.Contains(ally7Level)) BuyAllyAbility(7, ally7Level); else BuyAlly(7, ally7Level + 1);
 				else if(currentGameAction.action == Action.BuyAlly8)
-					BuyAlly(8, ally8Level + 1);
+					if(IsAbilityBuyWall(ally8Level) && !ally8Upgrades.Contains(ally8Level)) BuyAllyAbility(8, ally8Level); else BuyAlly(8, ally8Level + 1);
 				else if(currentGameAction.action == Action.BuyAlly9)
-					BuyAlly(9, ally9Level + 1);
+					if(IsAbilityBuyWall(ally9Level) && !ally9Upgrades.Contains(ally9Level)) BuyAllyAbility(9, ally9Level); else BuyAlly(9, ally9Level + 1);
 				else if(currentGameAction.action == Action.BuyAlly10)
-					BuyAlly(10, ally10Level + 1);
+					if(IsAbilityBuyWall(ally10Level) && !ally10Upgrades.Contains(ally10Level)) BuyAllyAbility(10, ally10Level); else BuyAlly(10, ally10Level + 1);
 				else if(currentGameAction.action == Action.BuyAlly11)
-					BuyAlly(11, ally11Level + 1);
+					if(IsAbilityBuyWall(ally11Level) && !ally11Upgrades.Contains(ally11Level)) BuyAllyAbility(11, ally11Level); else BuyAlly(11, ally11Level + 1);
 				else if(currentGameAction.action == Action.BuyAlly12)
-					BuyAlly(12, ally12Level + 1);
+					if(IsAbilityBuyWall(ally12Level) && !ally12Upgrades.Contains(ally12Level)) BuyAllyAbility(12, ally12Level); else BuyAlly(12, ally12Level + 1);
 				else if(currentGameAction.action == Action.BuyAlly13)
-					BuyAlly(13, ally13Level + 1);
+					if(IsAbilityBuyWall(ally13Level) && !ally13Upgrades.Contains(ally13Level)) BuyAllyAbility(13, ally13Level); else BuyAlly(13, ally13Level + 1);
 				else if(currentGameAction.action == Action.BuyAlly14)
-					BuyAlly(14, ally14Level + 1);
+					if(IsAbilityBuyWall(ally14Level) && !ally14Upgrades.Contains(ally14Level)) BuyAllyAbility(14, ally14Level); else BuyAlly(14, ally14Level + 1);
 				else if(currentGameAction.action == Action.BuyAlly15)
-					BuyAlly(15, ally15Level + 1);
+					if(IsAbilityBuyWall(ally15Level) && !ally15Upgrades.Contains(ally15Level)) BuyAllyAbility(15, ally15Level); else BuyAlly(15, ally15Level + 1);
 				else if(currentGameAction.action == Action.BuyAlly16)
-					BuyAlly(16, ally16Level + 1);
+					if(IsAbilityBuyWall(ally16Level) && !ally16Upgrades.Contains(ally16Level)) BuyAllyAbility(16, ally16Level); else BuyAlly(16, ally16Level + 1);
 				else if(currentGameAction.action == Action.BuyAlly17)
-					BuyAlly(17, ally17Level + 1);
+					if(IsAbilityBuyWall(ally17Level) && !ally17Upgrades.Contains(ally17Level)) BuyAllyAbility(17, ally17Level); else BuyAlly(17, ally17Level + 1);
 				else if(currentGameAction.action == Action.BuyAlly18)
-					BuyAlly(18, ally18Level + 1);
+					if(IsAbilityBuyWall(ally18Level) && !ally18Upgrades.Contains(ally18Level)) BuyAllyAbility(18, ally18Level); else BuyAlly(18, ally18Level + 1);
 				else if(currentGameAction.action == Action.BuyAlly19)
-					BuyAlly(19, ally19Level + 1);
+					if(IsAbilityBuyWall(ally19Level) && !ally19Upgrades.Contains(ally19Level)) BuyAllyAbility(19, ally19Level); else BuyAlly(19, ally19Level + 1);
 				else if(currentGameAction.action == Action.BuyAlly20)
-					BuyAlly(20, ally20Level + 1);
+					if(IsAbilityBuyWall(ally20Level) && !ally20Upgrades.Contains(ally20Level)) BuyAllyAbility(20, ally20Level); else BuyAlly(20, ally20Level + 1);
 
 				CheckActionCompletion();
 			}
@@ -667,6 +711,188 @@ public class CalculateManager : MonoBehaviour {
 		}
 	}
 
+	public bool IsAbilityBuyWall(int level)
+	{
+		if(level == 10 || level == 25 || level == 50 || level == 100)
+		{
+			return true;
+		}
+		else
+			return false;
+		
+	}
+
+	public void BuyAllyAbility(int allyId, int abilityAtLevel)
+	{
+		int displayHr = Mathf.RoundToInt(secsElapsed) / 3600;
+		int displayMins = Mathf.RoundToInt(secsElapsed) / 60 % 60;
+		int displaySecs = Mathf.RoundToInt(secsElapsed) % 60;
+
+		float multiplier = 1.5f;
+
+		AllyStatsRow meh = AllyStats.Instance.GetRow("ID_" + (abilityAtLevel-1));
+
+		if(allyId == 1 && meh._ally1Cost * multiplier <= currentMoney)
+		{
+			Debug.Log("COST ALLY 1 Ability Upgrade " + meh._ally1Cost * multiplier);
+			currentMoney -= meh._ally1Cost * multiplier;
+			ally1Upgrades.Add(abilityAtLevel);
+			if(displayAllyBought)
+				Debug.Log("Ally 1 [ABILITY] PURCHASED! Level: " + ally1Level + ", Curr Time: " + displayHr + "h " + displayMins + "m " + displaySecs + "s");
+		}
+		else if(allyId == 2 && meh._ally2Cost * multiplier <= currentMoney)
+		{
+			currentMoney -= meh._ally2Cost * multiplier;
+			ally2Upgrades.Add(abilityAtLevel);
+			if(displayAllyBought)
+				Debug.Log("Ally 2 [ABILITY] PURCHASED!! Level: " + ally2Level + ", Curr Time: " + displayHr + "h " + displayMins + "m " + displaySecs + "s");
+
+		}
+		else if(allyId == 3 && meh._ally3Cost * multiplier <= currentMoney)
+		{
+			currentMoney -= meh._ally3Cost * multiplier;
+			ally3Upgrades.Add(abilityAtLevel);
+			if(displayAllyBought)
+				Debug.Log("Ally 3 [ABILITY] PURCHASED! Level: " + ally3Level + ", Curr Time: " + displayHr + "h " + displayMins + "m " + displaySecs + "s");
+		}
+		else if(allyId == 4 && meh._ally4Cost * multiplier <= currentMoney)
+		{
+			currentMoney -= meh._ally4Cost * multiplier;
+			ally4Upgrades.Add(abilityAtLevel);
+			if(displayAllyBought)
+				Debug.Log("Ally 4 [ABILITY] PURCHASED! Level: " + ally4Level + ", Curr Time: " + displayHr + "h " + displayMins + "m " + displaySecs + "s");
+
+		}
+		else if(allyId == 5 && meh._ally5Cost * multiplier <= currentMoney)
+		{
+			currentMoney -= meh._ally5Cost * multiplier;
+			ally5Upgrades.Add(abilityAtLevel);
+			if(displayAllyBought)
+				Debug.Log("Ally 5 [ABILITY] PURCHASED! Level: " + ally5Level + ", Curr Time: " + displayHr + "h " + displayMins + "m " + displaySecs + "s");
+
+		}
+		else if(allyId == 6 && meh._ally6Cost * multiplier <= currentMoney)
+		{
+			currentMoney -= meh._ally6Cost * multiplier;
+			ally6Upgrades.Add(abilityAtLevel);
+			if(displayAllyBought)
+				Debug.Log("Ally 6 [ABILITY] PURCHASED! Level: " + ally6Level + ", Curr Time: " + displayHr + "h " + displayMins + "m " + displaySecs + "s");
+
+		}
+		else if(allyId == 7 && meh._ally7Cost * multiplier <= currentMoney)
+		{
+			currentMoney -= meh._ally7Cost * multiplier;
+			ally7Upgrades.Add(abilityAtLevel);
+			if(displayAllyBought)
+				Debug.Log("Ally 7 [ABILITY] PURCHASED! Level: " + ally7Level + ", Curr Time: " + displayHr + "h " + displayMins + "m " + displaySecs + "s");
+
+		}
+		else if(allyId == 8 && meh._ally8Cost * multiplier <= currentMoney)
+		{
+			currentMoney -= meh._ally8Cost * multiplier;
+			ally8Upgrades.Add(abilityAtLevel);
+			if(displayAllyBought)
+				Debug.Log("Ally 8 [ABILITY] PURCHASED! Level: " + ally8Level + ", Curr Time: " + displayHr + "h " + displayMins + "m " + displaySecs + "s");
+
+		}
+		else if(allyId == 9 && meh._ally9Cost * multiplier <= currentMoney)
+		{
+			currentMoney -= meh._ally9Cost * multiplier;
+			ally9Upgrades.Add(abilityAtLevel);
+			if(displayAllyBought)
+				Debug.Log("Ally 9 [ABILITY] PURCHASED! Level: " + ally9Level + ", Curr Time: " + displayHr + "h " + displayMins + "m " + displaySecs + "s");
+
+		}
+		else if(allyId == 10 && meh._ally10Cost * multiplier <= currentMoney)
+		{
+			currentMoney -= meh._ally10Cost * multiplier;
+			ally10Upgrades.Add(abilityAtLevel);
+			if(displayAllyBought)
+				Debug.Log("Ally 10 [ABILITY] PURCHASED! Level: " + ally10Level + ", Curr Time: " + displayHr + "h " + displayMins + "m " + displaySecs + "s");
+
+		}
+		else if(allyId == 11 && meh._ally11Cost * multiplier <= currentMoney)
+		{
+			currentMoney -= meh._ally11Cost * multiplier;
+			ally11Upgrades.Add(abilityAtLevel);
+			if(displayAllyBought)
+				Debug.Log("Ally 11 [ABILITY] PURCHASED! Level: " + ally11Level + ", Curr Time: " + displayHr + "h " + displayMins + "m " + displaySecs + "s");
+
+		}
+		else if(allyId == 12 && meh._ally12Cost * multiplier <= currentMoney)
+		{
+			currentMoney -= meh._ally12Cost * multiplier;
+			ally12Upgrades.Add(abilityAtLevel);
+			if(displayAllyBought)
+				Debug.Log("Ally 12 [ABILITY] PURCHASED! Level: " + ally12Level + ", Curr Time: " + displayHr + "h " + displayMins + "m " + displaySecs + "s");
+
+		}
+		else if(allyId == 13 && meh._ally13Cost * multiplier <= currentMoney)
+		{
+			currentMoney -= meh._ally13Cost * multiplier;
+			ally13Upgrades.Add(abilityAtLevel);
+			if(displayAllyBought)
+				Debug.Log("Ally 13 [ABILITY] PURCHASED! Level: " + ally13Level + ", Curr Time: " + displayHr + "h " + displayMins + "m " + displaySecs + "s");
+
+		}
+		else if(allyId == 14 && meh._ally14Cost * multiplier <= currentMoney)
+		{
+			currentMoney -= meh._ally14Cost * multiplier;
+			ally14Upgrades.Add(abilityAtLevel);
+			if(displayAllyBought)
+				Debug.Log("Ally 14 [ABILITY] PURCHASED! Level: " + ally14Level + ", Curr Time: " + displayHr + "h " + displayMins + "m " + displaySecs + "s");
+
+		}
+		else if(allyId == 15 && meh._ally15Cost * multiplier <= currentMoney)
+		{
+			currentMoney -= meh._ally15Cost * multiplier;
+			ally15Upgrades.Add(abilityAtLevel);
+			if(displayAllyBought)
+				Debug.Log("Ally 15 [ABILITY] PURCHASED! Level: " + ally15Level + ", Curr Time: " + displayHr + "h " + displayMins + "m " + displaySecs + "s");
+
+		}
+		else if(allyId == 16 && meh._ally16Cost * multiplier <= currentMoney)
+		{
+			currentMoney -= meh._ally16Cost * multiplier;
+			ally16Upgrades.Add(abilityAtLevel);
+			if(displayAllyBought)
+				Debug.Log("Ally 16 [ABILITY] PURCHASED! Level: " + ally16Level + ", Curr Time: " + displayHr + "h " + displayMins + "m " + displaySecs + "s");
+
+		}
+		else if(allyId == 17 && meh._ally17Cost * multiplier <= currentMoney)
+		{
+			currentMoney -= meh._ally17Cost * multiplier;
+			ally17Upgrades.Add(abilityAtLevel);
+			if(displayAllyBought)
+				Debug.Log("Ally 17 [ABILITY] PURCHASED! Level: " + ally17Level + ", Curr Time: " + displayHr + "h " + displayMins + "m " + displaySecs + "s");
+
+		}
+		else if(allyId == 18 && meh._ally18Cost * multiplier <= currentMoney)
+		{
+			currentMoney -= meh._ally18Cost * multiplier;
+			ally18Upgrades.Add(abilityAtLevel);
+			if(displayAllyBought)
+				Debug.Log("Ally 18 [ABILITY] PURCHASED! Level: " + ally18Level + ", Curr Time: " + displayHr + "h " + displayMins + "m " + displaySecs + "s");
+
+		}
+		else if(allyId == 19 && meh._ally19Cost * multiplier <= currentMoney)
+		{
+			currentMoney -= meh._ally19Cost * multiplier;
+			ally19Upgrades.Add(abilityAtLevel);
+			if(displayAllyBought)
+				Debug.Log("Ally 19 [ABILITY] PURCHASED! Level: " + ally19Level + ", Curr Time: " + displayHr + "h " + displayMins + "m " + displaySecs + "s");
+
+		}
+		else if(allyId == 20 && meh._ally20Cost * multiplier <= currentMoney)
+		{
+			currentMoney -= meh._ally20Cost * multiplier;
+			ally20Upgrades.Add(abilityAtLevel);
+			if(displayAllyBought)
+				Debug.Log("Ally 20 [ABILITY] PURCHASED! Level: " + ally20Level + ", Curr Time: " + displayHr + "h " + displayMins + "m " + displaySecs + "s");
+
+		}
+	}
+
 	[System.Serializable]
 	public class GameActions
 	{
@@ -700,6 +926,12 @@ public class CalculateManager : MonoBehaviour {
 		BuyAlly20,
 		SetTapPerSecond,
 		ReachStageNumber
+	}
+
+	public enum AIType
+	{
+		CustomSet,
+		OneGarfieldTwoAllies
 	}
 
 
